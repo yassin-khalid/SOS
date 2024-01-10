f234a4265f10f172e176b2722f27751dfef8f45f
Yassin Rahman <ykfathulrahman@gmail.com>
2024-01-10 14:13:44 +0300

    adding a toaster to landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,10 +4,14 @@ import Header from "@/components/Header";
 import Objectives from "@/components/Objectives";
 import Vision from "@/components/Vision";
 import Who from "@/components/Who";
+import { Toaster } from "@/components/ui/sonner";
+import { useTranslation } from "react-i18next";
 
 const Landing = () => {
+  const { i18n } = useTranslation();
   return (
     <>
+      <Toaster dir={i18n.resolvedLanguage === "en" ? "ltr" : "rtl"} />
       <Header />
       <Vision />
       <Who />
