f1716e7cc0dde5867e37121082eb39ad803b7513
Yassin Rahman <ykfathulrahman@gmail.com>
2024-01-10 14:06:29 +0300

    adding terms & policies routes and make footer as a common ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { ThemeProvider } from "./components/theme-provider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Footer from "./components/Footer";
+import PrivacyPage from "./pages/PrivacyPage";
+import TermsPage from "./pages/TermsPage";
 
 function App() {
   return (
@@ -8,7 +11,17 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          <Route
+            path="/customer-terms"
+            element={<TermsPage terms="customer-terms" />}
+          />
+          <Route
+            path="/service-terms"
+            element={<TermsPage terms="service-terms" />}
+          />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </ThemeProvider>
   );
