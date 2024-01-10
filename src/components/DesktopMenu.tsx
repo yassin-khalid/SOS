ba72ad45f0786f994e376b8418f402f28217fd16
Yassin Rahman <ykfathulrahman@gmail.com>
2024-01-10 14:07:57 +0300

    refactor navigation links paths

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -10,22 +10,22 @@ const DesktopMenu = () => {
     >
       <ul className="flex items-center gap-8">
         <li>
-          <a className="p-3 hover:text-primary rounded" href="#">
+          <a className="p-3 hover:text-primary rounded" href="/">
             {t("nav.home")}
           </a>
         </li>
         <li>
-          <a className="p-3 hover:text-primary rounded" href="#">
+          <a className="p-3 hover:text-primary rounded" href="/#about">
             {t("nav.about")}
           </a>
         </li>
         <li>
-          <a className="p-3 hover:text-primary rounded" href="#">
+          <a className="p-3 hover:text-primary rounded" href="/#contact">
             {t("nav.contact")}
           </a>
         </li>
         <li>
-          <a className="p-3 hover:text-primary rounded" href="#">
+          <a className="p-3 hover:text-primary rounded" href="/#track">
             {t("nav.track")}
           </a>
         </li>
