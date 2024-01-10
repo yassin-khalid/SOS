0a6510693a407aef264414376d907147397c8f43
Yassin Rahman <ykfathulrahman@gmail.com>
2024-01-10 14:09:19 +0300

    derive main nav from header as a different component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,63 +2,12 @@ import logo from "../assets/sos-logo.png";
 import { useEffect, useRef } from "react";
 import { ModeToggle } from "./mode-toggle";
 import Hero from "./Hero";
-import DesktopMenu from "./DesktopMenu";
-import MobileMenu from "./MobileMenu";
-import { useTranslation } from "react-i18next";
-import LanguageToggle from "./LanguageToggle";
-import Social from "./Social";
+import MainNav from "./MainNav";
 
 const Header = () => {
-  const ref = useRef(document.createElement("div"));
-  const { i18n } = useTranslation();
-
-  useEffect(() => {
-    const scroll = () => {
-      ref.current.classList.toggle("fixed", window.scrollY >= 250);
-      ref.current.classList.toggle("bg-white", window.scrollY >= 250);
-      ref.current.classList.toggle("z-10", window.scrollY >= 250);
-      ref.current.classList.toggle("shadow", window.scrollY >= 250);
-      ref.current.classList.toggle("w-full", window.scrollY >= 250);
-      ref.current.classList.toggle("dark:bg-background", window.scrollY >= 250);
-      ref.current.classList.toggle(
-        "dark:text-secondary",
-        window.scrollY >= 250
-      );
-    };
-    window.addEventListener("scroll", scroll);
-
-    return () => {
-      window.removeEventListener("scroll", scroll);
-    };
-  }, []);
-
   return (
     <header className="h-screen bg-hero bg-center bg-cover relative">
-      <div ref={ref} className="py-3 text-darker">
-        <div
-          className="container max-w-6xl flex items-center justify-between"
-          dir={i18n.resolvedLanguage === "en" ? "ltr" : "rtl"}
-        >
-          <a href="#">
-            <img className="w-10" src={logo} alt="" />
-          </a>
-          <div
-            className="flex items-center gap-8"
-            dir={i18n.resolvedLanguage === "en" ? "ltr" : "rtl"}
-          >
-            <DesktopMenu />
-            <div
-              className="flex items-center gap-2"
-              dir={i18n.resolvedLanguage === "en" ? "ltr" : "rtl"}
-            >
-              <ModeToggle />
-              <Social />
-              <LanguageToggle />
-              <MobileMenu />
-            </div>
-          </div>
-        </div>
-      </div>
+      <MainNav />
       <Hero />
     </header>
   );
---
+++ src/components/MainNav.tsx
@@ -0,0 +1,60 @@
+import logo from "../assets/sos-logo.png";
+import { useEffect, useRef } from "react";
+import { ModeToggle } from "./mode-toggle";
+import DesktopMenu from "./DesktopMenu";
+import MobileMenu from "./MobileMenu";
+import { useTranslation } from "react-i18next";
+
+const MainNav = () => {
+  const ref = useRef(document.createElement("div"));
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    const scroll = () => {
+      ref.current.classList.toggle("fixed", window.scrollY >= 250);
+      ref.current.classList.toggle("bg-white", window.scrollY >= 250);
+      ref.current.classList.toggle("z-10", window.scrollY >= 250);
+      ref.current.classList.toggle("shadow", window.scrollY >= 250);
+      ref.current.classList.toggle("w-full", window.scrollY >= 250);
+      ref.current.classList.toggle("dark:bg-background", window.scrollY >= 250);
+      ref.current.classList.toggle(
+        "dark:text-secondary",
+        window.scrollY >= 250
+      );
+    };
+    window.addEventListener("scroll", scroll);
+
+    return () => {
+      window.removeEventListener("scroll", scroll);
+    };
+  }, []);
+  return (
+    <div ref={ref} className="py-3 text-darker">
+      <div
+        className="container max-w-6xl flex items-center justify-between"
+        dir={i18n.resolvedLanguage === "en" ? "ltr" : "rtl"}
+      >
+        <a href="/">
+          <img className="w-10" src={logo} alt="" />
+        </a>
+        <div
+          className="flex items-center gap-8"
+          dir={i18n.resolvedLanguage === "en" ? "ltr" : "rtl"}
+        >
+          <DesktopMenu />
+          <div
+            className="flex items-center gap-2"
+            dir={i18n.resolvedLanguage === "en" ? "ltr" : "rtl"}
+          >
+            <ModeToggle />
+            {/* <Social /> */}
+            {/* <LanguageToggle /> */}
+            <MobileMenu />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainNav;
