f7aa649a57be851b5f7e6668d93cbe30340da32e
Yassin Rahman <ykfathulrahman@gmail.com>
2024-01-10 14:10:02 +0300

    remove unused imports

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,3 @@
-import logo from "../assets/sos-logo.png";
-import { useEffect, useRef } from "react";
-import { ModeToggle } from "./mode-toggle";
 import Hero from "./Hero";
 import MainNav from "./MainNav";
 
